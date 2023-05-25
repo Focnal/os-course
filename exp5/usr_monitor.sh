e12905f3f867ea0504bfdcbe47b343d130c9d941
Ghost White <2673953698@qq.com>
2023-05-25 19:33:04 +0800

    改进了实验五的实现

--- exp5/usr_monitor.sh
+++ exp5/usr_monitor.sh
@@ -6,24 +6,47 @@ then
 fi
 
 user=$1
-
-if [[ $user == $(who | grep -w $user | awk '{print $1}') ]];
-then
-    echo "用户[$user]已经登录！"
-    echo "按任意键退出"
-    read -n1 -r -p ""
-    exit 0
-fi
-
+logged_in_users=$(who | grep -w $user | awk '{print $1}')
+logged_in_ports=$(who | grep -w $user | awk '{print $2}')
+# 如果用户在多个位置上登录， 就挨个检查一遍
+for logged_in_user in $logged_in_users
+do
+    if [[ $user == $logged_in_user ]];
+    then
+        echo -n "用户[$user]在[ "
+        for logged_in_port in $logged_in_ports
+        do
+            echo -n "$logged_in_port "
+        done 
+        echo "]上登录"
+        echo "按任意键退出"
+        read -n1 -r -p ""
+        exit 0
+    fi
+done
 
 while true;
 do
     echo "等待用户[$user]登录..."
-    if [[ $user == $(who | grep -w $user | awk '{print $1}') ]];
-    then
-        echo "用户[$user]已经登录！"
-    break
-    fi
+    logged_in_users=$(who | grep -w $user | awk '{print $1}')
+    logged_in_ports=$(who | grep -w $user | awk '{print $2}')
+    # 如果用户在多个位置上登录， 就挨个检查一遍
+    for logged_in_user in $logged_in_users
+    do
+        if [[ $user == $logged_in_user ]];
+        then
+            echo -n "用户[$user]在[ "
+            for logged_in_port in $logged_in_ports
+            do
+                echo -n "$logged_in_port "
+            done 
+            echo "]上登录"
+            echo "按任意键退出"
+            read -n1 -r -p ""
+            exit 0
+        fi
+    done
+
     sleep 1
 done
 
